70be79b05c1b2be40a89170a3245b18069cfa964
Edinson Cotrina Ramos <ecotrina106@gmail.com>
2024-08-16 13:08:56 -0500

    Refactoring de método de generateReport

--- src/main/java/com/app/service/impl/InvoiceServiceImpl.java
+++ src/main/java/com/app/service/impl/InvoiceServiceImpl.java
@@ -1,6 +1,7 @@
 package com.app.service.impl;
 
 import com.app.model.Invoice;
+import com.app.model.InvoiceDetail;
 import com.app.repo.IClientRepo;
 import com.app.repo.IDishRepo;
 import com.app.repo.IInvoiceRepo;
@@ -14,6 +15,7 @@ import reactor.core.publisher.Mono;
 
 import java.io.InputStream;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -29,9 +31,74 @@ public class InvoiceServiceImpl extends CRUDImpl<Invoice,String> implements IInv
         return invoiceRepo;
     }
 
+    private Mono<Invoice> poblarCliente(Invoice invoice){
+        return clientRepo.findById(invoice.getClient().getId())
+                .map(client -> {
+                    invoice.setClient(client);
+                    return invoice;
+                });
+    }
+
+    private Mono<Invoice> poblarItems(Invoice invoice){
+        List<Mono<InvoiceDetail>> list = invoice.getItems().stream()// Stream<InvoiceDetail>
+                .map(detail -> dishRepo.findById(detail.getDish().getId())//Mono<Dish>
+                        .map(dish -> {
+                            detail.setDish(dish);
+                            return detail;
+                        })
+                ).toList();
+
+        //Mono when espera a que todos de la lista se procese para ejecutar then
+        return Mono.when(list).then(Mono.just(invoice));
+    }
+
+    private byte[] generarPDF(Invoice invoice){
+            try ( InputStream stream = getClass().getResourceAsStream("/facturas.jrxml")){
+                Map<String,Object> params = new HashMap<>();
+                params.put("txt_client",invoice.getClient().getFirstName()+" "+invoice.getClient().getLastName());
+
+                JasperReport jasper = JasperCompileManager.compileReport(stream);
+                JasperPrint print = JasperFillManager.fillReport(jasper,params,new JRBeanCollectionDataSource(invoice.getItems()));
+                return  JasperExportManager.exportReportToPdf(print);
+            } catch (Exception e) {
+                return new byte[0];
+            }
+    }
+
     @Override
     public Mono<byte[]> generateReport(String idInvoice) {
         return invoiceRepo.findById(idInvoice)
+                .flatMap(this::poblarCliente)
+                .flatMap(this::poblarItems)
+                .map(this::generarPDF)
+                .onErrorResume(e->Mono.empty());
+    }
+
+   /* @Override
+    public Mono<byte[]> generateReport(String idInvoice) {
+        return invoiceRepo.findById(idInvoice)
+                //Obteniendo Client
+                .flatMap(invoice -> Mono.just(invoice)
+                            .zipWith(clientRepo.findById(invoice.getClient().getId()), (inv, client)-> {
+                                inv.setClient(client);
+                                return inv;
+                            })
+                )
+                //Obteniendo cada Dish
+                .flatMap(invoice -> {
+                    return Flux.fromIterable(invoice.getItems())
+                            .flatMap(detail ->{
+                                return dishRepo.findById(detail.getDish().getId())
+                                        .map( dish -> {
+                                            detail.setDish(dish);
+                                            return detail;
+                                        });
+                            }).collectList()
+                            .flatMap(list ->{
+                                invoice.setItems(list);
+                                return Mono.just(invoice);
+                            });
+                })
                 .map(inv -> {
                     try{
                         Map<String,Object> params = new HashMap<>();
@@ -45,5 +112,5 @@ public class InvoiceServiceImpl extends CRUDImpl<Invoice,String> implements IInv
                         return new byte[0];
                     }
                 });
-    }
+    }*/
 }
